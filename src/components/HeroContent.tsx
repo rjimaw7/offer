9d51a17a6a1c33be7048a1302209c92a69250018
rjimaw7 <rjimaw7777@gmail.com>
2024-06-08 15:29:13 +0800

    ui adjustments

--- src/components/HeroContent.tsx
+++ src/components/HeroContent.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import HeroLogo from "@/assets/herologo.png";
 
 const HeroContent = () => {
   return (
@@ -7,7 +8,7 @@ const HeroContent = () => {
       <div className="">
         <div className="relative w-[420px] h-[550px]">
           <Image
-            src="/herologo.png"
+            src={HeroLogo}
             fill
             alt="herologo"
             loading="lazy"
@@ -15,25 +16,28 @@ const HeroContent = () => {
           />
         </div>
       </div>
-      <div className="grid grid-cols-1">
-        <div className=" flex items-center justify-start -translate-x-40 2xl:-translate-x-72 3xl:-translate-x-96">
-          <h1 className="text-2xl 2xl:text-4xl scale-125 herologo text-white leading-snug 3xl:w-[90%]">
+      <div className="grid grid-cols-1 -translate-x-48 2xl:-translate-x-72 w-[500px] 2xl:w-full 3xl:w-[600px]">
+        <div className="flex items-center justify-start">
+          <h1 className="text-4xl herologo 2xl:scale-125 3xl:scale-150 text-white leading-snug">
             Turn Your Website Into Your Best Sales Person
           </h1>
         </div>
-        <div className="-translate-x-2 2xl:-translate-x-24 flex flex-col gap-4 pt-12 3xl:-translate-x-48">
-          <p className="font-rubik text-[#F7F7FF] text-base 2xl:text-lg leading-relaxed font-light">
-            Triple your sales with a pixel-perfect website. At SmartSite Studio,
-            we don’t just design and write code, we build a revenue-driving
-            machine for your business.
-          </p>
-          <div className="text-white flex items-center gap-4">
-            <p className="border border-transparent rounded-full bg-[#FF632C] px-5 py-3 2xl:px-10 2xl:py-3 btntext cursor-pointer">
-              Talk to Us
+        <div className="pt-12 grid grid-cols-3 2xl:grid-cols-1 ">
+          <div className="col-span-1 2xl:hidden"></div>
+          <div className="col-span-2 2xl:col-span-1 flex flex-col gap-6 2xl:translate-x-48">
+            <p className="font-rubik text-[#F7F7FF] text-base 2xl:text-lg leading-relaxed font-light 2xl:w-full">
+              Triple your sales with a pixel-perfect website. At SmartSite
+              Studio, we don’t just design and write code, we build a
+              revenue-driving machine for your business.
             </p>
-            <p className="border border-[#0083FE] rounded-full bg-transparent px-5 py-3 2xl:px-10 2xl:py-3 btntext cursor-pointer hover:bg-[#FF632C] hover:border-[#FF632C]">
-              See Portfolio
-            </p>
+            <div className="text-white flex items-center gap-4">
+              <p className="border border-transparent rounded-full bg-[#FF632C] px-5 py-3 2xl:px-10 2xl:py-3 btntext cursor-pointer">
+                Talk to Us
+              </p>
+              <p className="border border-[#0083FE] rounded-full bg-transparent px-5 py-3 2xl:px-10 2xl:py-3 btntext cursor-pointer hover:bg-[#FF632C] hover:border-[#FF632C]">
+                See Portfolio
+              </p>
+            </div>
           </div>
         </div>
       </div>
